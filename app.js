73040b599e409a16a114d92471ba83056a6f5c57
Kakamotobi <daeram.chung@gmail.com>
2021-07-13 03:12:23 +0900

    [JS] Add comicbooks feature

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("form");
 const heroContainer = document.querySelector(".hero-container");
 const comicsContainer = document.querySelector(".comics-container");
+const h1 = document.querySelector("h1");
 
 // Search Hero
 form.addEventListener("submit", function (evt) {
@@ -38,11 +39,50 @@ form.addEventListener("submit", function (evt) {
 							? "Sorry. No Description Avabilable."
 							: description
 					}</p>
+					<p class="copyright">${attributionText}</p>
 				</div>
-				<p class="copyright">${attributionText}</p>
 			`;
+
+			const comicsAPI = `https://gateway.marvel.com:443/v1/public/characters/${id}/comics?apikey=39412169a8ad74430a18f462d40b6ec8`;
+			return fetch(comicsAPI);
+		})
+		.then((res) => {
+			return res.json();
+		})
+		.then((comicBooks) => {
+			const { results } = comicBooks.data;
+
+			comicsContainer.innerHTML = "";
+
+			const h2 = document.createElement("h2");
+			h2.innerText = "Comic Books";
+			h2.classList.add("comics-container__title");
+			comicsContainer.append(h2);
+			comicsContainer.style.backgroundColor = "var(--dark)";
+
+			const comicsWrapper = document.createElement("div");
+			comicsWrapper.classList.add("comics-wrapper");
+
+			for (let result of results) {
+				const img = document.createElement("img");
+				img.src = `${result.thumbnail.path}.${result.thumbnail.extension}`;
+				img.alt = `${result.title} Thumbnail`;
+				img.classList.add("comic__thumbnail");
+				const title = document.createElement("h4");
+				title.innerText = result.title;
+				title.classList.add("comic__title");
+				const comic = document.createElement("div");
+				comic.classList.add("comic");
+				comic.append(img, title);
+				comicsWrapper.append(comic);
+			}
+			comicsContainer.append(comicsWrapper);
 		})
 		.catch((err) => {
-			console.log("Something went wrong with fetch", err);
+			heroContainer.innerHTML = `
+			<h1>Sorry. We couldn't find your hero.</h1>
+			`;
+			comicsContainer.innerHTML = "";
+			comicsContainer.style.backgroundColor = "";
 		});
 });
